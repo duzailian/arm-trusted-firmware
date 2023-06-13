818e5640062867695d38e688f78c9fbfdee8a97f
duzailian <duzailian@qq.com>
2023-06-13 01:52:37 -0700

    1. 编译支持PIE的bl31的命令到build.sh(因未知原因无法链接通过，暂未使能)

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export CROSS_COMPILE=/opt/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
 __CC=`which clang`
+# 编译支持PIE的bl31
+#make CC=${__CC} PLAT=fvp RESET_TO_BL31=1 ENABLE_PIE=1 bl31
 make CC=${__CC} PLAT=fvp all
