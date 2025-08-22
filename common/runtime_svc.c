612f8a4574b1f8c93b9990789bfe4a091b0cbff5
Luojun <2369570786@qq.com>
2025-08-22 10:13:23 +0800

    学习笔记更新 1.将study_notes更名为学习笔记,供大家归类学习成果. 2.对runtime_svc.c中的运行时服务初始化模块添加注释. 3.上传ATF各阶段代码启动流程图笔记，可用visio直接打开. 4.上传官网文档学习思维导图笔记，后续会一直更新.
    Signed-off-by: Luojun <2369570786@qq.com>

--- common/runtime_svc.c
+++ common/runtime_svc.c
@@ -20,10 +20,10 @@
  * 'rt_svc_descs_indices' array. This gives the index of the descriptor in the
  * 'rt_svc_descs' array which contains the SMC handler.
  ******************************************************************************/
-uint8_t rt_svc_descs_indices[MAX_RT_SVCS];
+uint8_t rt_svc_descs_indices[MAX_RT_SVCS];/*运行服务索引数组、OEN+调用类型最多128种*/
 
 #define RT_SVC_DECS_NUM		((RT_SVC_DESCS_END - RT_SVC_DESCS_START)\
-					/ sizeof(rt_svc_desc_t))
+					/ sizeof(rt_svc_desc_t))/*单位:字节数，计算注册的运行时服务数*/
 
 /*******************************************************************************
  * Function to invoke the registered `handle` corresponding to the smc_fid in
@@ -95,17 +95,18 @@ void __init runtime_svc_init(void)
 
 	/* Assert the number of descriptors detected are less than maximum indices */
 	assert((RT_SVC_DESCS_END >= RT_SVC_DESCS_START) &&
-			(RT_SVC_DECS_NUM < MAX_RT_SVCS));
+			(RT_SVC_DECS_NUM < MAX_RT_SVCS));/*判断链接脚本导出描述符区间是否合法，服务数最大为128*/
 
 	/* If no runtime services are implemented then simply bail out */
 	if (RT_SVC_DECS_NUM == 0U) {
-		return;
+		return;/*没有实现运行服务则直接return*/
 	}
 	/* Initialise internal variables to invalid state */
+	/*将服务索引数组成员全部初始化为FF,避免随机值导致误用，void显式忽略返回值*/
 	(void)memset(rt_svc_descs_indices, -1, sizeof(rt_svc_descs_indices));
 
-	rt_svc_descs = (rt_svc_desc_t *) RT_SVC_DESCS_START;
-	for (index = 0U; index < RT_SVC_DECS_NUM; index++) {
+	rt_svc_descs = (rt_svc_desc_t *) RT_SVC_DESCS_START;/*强转一下，将描述符数组首地址指向第一个服务的地址*/
+	for (index = 0U; index < RT_SVC_DECS_NUM; index++) {/*逐个遍历所有运行时服务*/
 		rt_svc_desc_t *service = &rt_svc_descs[index];
 
 		/*
@@ -113,11 +114,11 @@ void __init runtime_svc_init(void)
 		 * difficult to predict the system behaviour in the absence
 		 * of this service.
 		 */
-		rc = validate_rt_svc_desc(service);
+		rc = validate_rt_svc_desc(service);/*检查当前描述符是否合法，此处考虑静态编译阶段的问题*/
 		if (rc != 0) {
 			ERROR("Invalid runtime service descriptor %p\n",
 				(void *) service);
-			panic();
+			panic();/*非法描述符一个都不允许存在，后续调进来会死机，直接崩溃*/
 		}
 
 		/*
@@ -127,12 +128,12 @@ void __init runtime_svc_init(void)
 		 * an initialisation routine defined. Call the initialisation
 		 * routine for this runtime service, if it is defined.
 		 */
-		if (service->init != NULL) {
+		if (service->init != NULL) {/*若服务的初始化函数非空，则执行初始化*/
 			rc = service->init();
 			if (rc != 0) {
 				ERROR("Error initializing runtime service %s\n",
 						service->name);
-				continue;
+				continue;/*初始化失败不影响下一次迭代，continue关键字跳过当前循环的剩余部分*/
 			}
 		}
 
@@ -143,7 +144,7 @@ void __init runtime_svc_init(void)
 		 * entity range.
 		 */
 		start_idx = (uint8_t)get_unique_oen(service->start_oen,
-						    service->call_type);
+						    service->call_type);/*强转为8位满足最大128的需求*/
 		end_idx = (uint8_t)get_unique_oen(service->end_oen,
 						  service->call_type);
 		assert(start_idx <= end_idx);
